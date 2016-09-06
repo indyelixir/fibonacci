1a1dc0790f04b4228656630bfd729f4ded85c620
Steve Grossi <me@stevegrossi.com>
2016-09-06 07:23:20 -0400

    Handle negative lengths with an error tuple

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -2,10 +2,11 @@ defmodule Fibonacci do
 
   def series(0), do: []
   def series(1), do: [0]
-  def series(length) do
+  def series(length) when length > 1 do
     [number_at_position(length) | Enum.reverse(series(length - 1))]
     |> Enum.reverse
   end
+  def series(_), do: {:error, "Length must not be negative"}
 
   defp number_at_position(1), do: 0
   defp number_at_position(2), do: 1
--- test/fibonacci_test.exs
+++ test/fibonacci_test.exs
@@ -7,4 +7,8 @@ defmodule FibonacciTest do
     assert Fibonacci.series(1) == [0]
     assert Fibonacci.series(8) == [0, 1, 1, 2, 3, 5, 8, 13]
   end
+
+  test "requires non-negative length" do
+    assert Fibonacci.series(-1) == {:error, "Length must not be negative"}
+  end
 end
