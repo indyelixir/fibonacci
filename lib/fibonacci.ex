25fc3b414be7f4a01199425466a125d5b511af60
Steve Grossi <me@stevegrossi.com>
2016-09-06 07:14:59 -0400

    Our first passing test
    This could certainly be improved with the addition of memoization and tail-call optimization. Soon!

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -1,2 +1,15 @@
 defmodule Fibonacci do
+
+  def series(0), do: []
+  def series(1), do: [0]
+  def series(length) do
+    [number_at_position(length) | Enum.reverse(series(length - 1))]
+    |> Enum.reverse
+  end
+
+  defp number_at_position(1), do: 0
+  defp number_at_position(2), do: 1
+  defp number_at_position(number) do
+    number_at_position(number - 2) + number_at_position(number - 1)
+  end
 end
--- test/fibonacci_test.exs
+++ test/fibonacci_test.exs
@@ -2,7 +2,9 @@ defmodule FibonacciTest do
   use ExUnit.Case
   doctest Fibonacci
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "returns list of specified length" do
+    assert Fibonacci.series(0) == []
+    assert Fibonacci.series(1) == [0]
+    assert Fibonacci.series(8) == [0, 1, 1, 2, 3, 5, 8, 13]
   end
 end
