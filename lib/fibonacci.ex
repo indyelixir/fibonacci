adbd7aca0aa0c9e6af228f53d104ba0fe3f150aa
Steve Grossi <me@stevegrossi.com>
2016-09-06 08:31:57 -0400

    Add module and function documentation

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -1,5 +1,17 @@
 defmodule Fibonacci do
+  @moduledoc """
+  Contains functions for working with the Fibonacci series.
+  """
 
+  @doc """
+  Returns a list of numbers in the Fibonacci series
+  of the specified length.
+
+  ## Example
+
+      iex> Fibonacci.series(5)
+      [0, 1, 1, 2, 3]
+  """
   def series(0), do: []
   def series(1), do: [0]
   def series(length) when length > 1 do
