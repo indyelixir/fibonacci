23e0713fa405c1a8721fadea823278ee89dcd19f
Joel Meador <joel@expectedbehavior.com>
2016-09-07 02:45:54 -0400

    simplify the calling interface to the private #compute function and let it handle correct return values via reverser

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -16,11 +16,11 @@ defmodule Fibonacci do
   def series(1), do: [0]
   def series(2), do: [0, 1]
   def series(length) when length > 2 and is_integer(length) do
-    compute([1,0], length - 2)
-    |> Enum.reverse
+    compute(length)
   end
   def series(_), do: {:error, "Length must be a non-negative integer"}
 
+  defp compute(length), do: compute([1, 0], length - 2) |> Enum.reverse
   defp compute(acc, 0), do: acc
   defp compute([n2, n1|_] = acc, remaining) do
     compute([n2 + n1 | acc], remaining - 1)
