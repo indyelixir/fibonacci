0049deaa681970736ba45c1907561808a48a87df
Joel Meador <joel@expectedbehavior.com>
2016-09-10 21:22:36 -0400

    add Fibonacci.number to compute the fibonacci at that position in the sequence

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -12,10 +12,28 @@ defmodule Fibonacci do
       iex> Fibonacci.series(5)
       [0, 1, 1, 2, 3]
   """
-  def series(length) when length >= 0 and is_integer(length) do
-    {0, 1}
-    |> Stream.unfold(fn({n1, n2}) -> {n1, {n2, n1+n2}} end)
+  def series(length) when is_integer(length) and length >= 0 do
+    stream
     |> Enum.take(length)
   end
-  def series(_), do: {:error, "Length must be a non-negative integer"}
+  def series(_), do: {:error, "`length` must be a non-negative integer"}
+
+  @doc """
+  Returns the Fibonacci number in the series at the
+  given 1-based `sequence` number
+  ## Example
+
+      iex> Fibonacci.number(2)
+      1
+  """
+  def number(sequence) when is_integer(sequence) and sequence > 0 do
+    stream
+    |> Enum.at(sequence - 1)
+  end
+  def number(_), do: {:error, "`sequence` must be a positive integer"}
+
+  defp stream, do: stream({0, 1})
+  defp stream(seed) do
+    Stream.unfold(seed, fn({n1, n2}) -> {n1, {n2, n1+n2}} end)
+  end
 end
